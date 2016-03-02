5d72a423cf41f6e1bd4fe1a20998b8834676134c
Megh Vakharia <meghhv@gmail.com>
2016-03-01 17:14:02 -0800

    remove jquery, remove moving gradient

--- js/main.js
+++ js/main.js
@@ -1,98 +1,20 @@
-$(document).ready(function() {
+// window.onload = function() {
+  
+//     var placeholder = document.querySelector('.placeholder'),
+//         small = placeholder.querySelector('.img-small')
 
-	// var scroll = new scroller().init();
+//     // 1: load small image and show it
+//     var img = new Image();
+//     img.src = small.src;
+//     img.onload = function () {
+//      small.classList.add('loaded');
+//     };
 
-	// $(window).resize(function() {
-	// 	scroll 
-	// });
-	// $(window).scroll(function(e) {
-	// 	// store starting position
-		
-	// 	sticky = false;
-
-
-	// 	// as it scrolls, if the top is >= top of original element
-
-	// 	// if it is sticky and it hits top of starting position, remove
-	// 	// stickiness
-	// 	if(sticky && $(this).scrollTop() <= startingPosition.top ) {
-	// 		sticky = false;
-	// 		$el.removeClass("sticky");
-	// 		console.log("no sticky");
-	// 	} else if(!sticky &&  $(this).scrollTop() > $el.offset().top) {
-	// 		sticky = true;
-	// 		$el.addClass("sticky");
-	// 		console.log("sticky");
-	// 	}
-
-
-
-	// });
-});
-
-var scroller = function() {
-	var navItem,
-	startingPosition,
-	sticky;
-
-	this.stickNav = function() {
-		if(typeof navItem == "undefined") { navItem = $(".title-nav.1"); }
-		if(typeof startingPosition == "undefined") { startingPosition = navItem.offset(); }
-		
-		$(window).scroll(function(e) {
-
-			// if not sticky, check if scrollTop > starting Position
-			if(!sticky && $(this).scrollTop() > startingPosition.top - 10) {
-				sticky = true;
-				navItem.addClass("sticky");
-				console.log("became sticky");
-
-
-
-			} else if(sticky && $(this).scrollTop() <= startingPosition.top - 10) {
-				sticky = false;
-				navItem.removeClass("sticky");
-				console.log("not sticky");
-			}
-
-
-
-		});
-	};
-
-	this.highlightSpecificPage = function() {
-
-		var highlighted = false;
-
-		$(window).scroll(function(e) {
-			if(!highlighted && navItem.offset().top >= $(".2-about").offset().top) {
-				$(".about").addClass("on-page");
-				highlighted = true;
-			} else if(highlighted && navItem.offset().top < $(".2-about").offset().top) {
-				$(".about").removeClass("on-page");
-				highlighted = false;
-			}
-		});
-	};
-
-	this.initVariables = function() {
-		navItem = $(".title-nav.1");
-		startingPosition = navItem.offset();
-	};
-
-	this.init = function() {
-		navItem = $(".title-nav.1");
-		startingPosition = navItem.offset();
-		sticky = false;
-
-		$(window).resize(function() {
-			this.initVariables();
-		});
-
-		this.stickNav();
-		this.highlightSpecificPage();
-		return this;
-	};
-
-};
-
+//     // 2: load large image
+//     var imgLarge = new Image();
+//     imgLarge.src = placeholder.dataset.large; 
+//     imgLarge.onload = function () {
+//       imgLarge.classList.add('loaded');
+//     };
+//     placeholder.appendChild(imgLarge);
+// }
